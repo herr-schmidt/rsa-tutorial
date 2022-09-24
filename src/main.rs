aa4ad2807f72144da3a16840de69617a742f2c8d
EinHerrSchmidt <matte24694@gmail.com>
2022-09-24 22:27:51 +0200

    Parallelism.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,54 @@ use std::ops::Div;
 use num_bigint::{BigUint, RandBigInt, ToBigUint};
 use num_traits::Zero;
 
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+use rand::SeedableRng;
+
 fn main() {
-    let (prime, trials) = find_prime();
-    println!("key = {}", prime);
-    println!("trials = {}", trials);
+    let r = rand::rngs::StdRng::seed_from_u64(6678235);
+    let result = Arc::new(Mutex::new((0.to_biguint().unwrap(), false, r)));
+    let mut handles = vec![];
+
+    for _ in 0..12 {
+        let result = Arc::clone(&result);
+        let handle = thread::spawn(move || loop {
+            let mut mutex_guard = result.lock().unwrap();
+            if mutex_guard.1 {
+                break;
+            }
+            
+            let candidate = mutex_guard.2.gen_biguint(1024);
+            std::mem::drop(mutex_guard);
+            let is_prime = miller_rabin(&candidate, &10);
+
+            if is_prime {
+                let mut mutex_guard = result.lock().unwrap();
+                mutex_guard.0 = candidate;
+                mutex_guard.1 = true;
+                break;
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("n: {}", (*result.lock().unwrap()).0);
 }
+
+// fn main() {
+//     let (prime, trials) = find_prime();
+//     println!("key = {}", prime);
+//     println!("trials = {}", trials);
+// }
 
 fn find_prime() -> (BigUint, u32) {
     let mut rng = rand::thread_rng();
-    let bit_size = 512;
+    let bit_size = 2048;
     let mut candidate = rng.gen_biguint(bit_size);
     let mut trials = 0;
     while !miller_rabin(&candidate, &10) {
@@ -42,9 +81,6 @@ fn miller_rabin(candidate: &BigUint, iterations: &u32) -> bool {
                 q = n;
                 break;
             }
-        }
-        if k == 0 {
-            return false;
         }
 
         let mut rng = rand::thread_rng();
